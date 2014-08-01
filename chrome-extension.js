0efe2156f256ebe5403e8ef8add99fdab1634b35
Matthew Kaufer <mjkaufer@gmail.com>
2014-07-31 22:48:18 -0400

    I hate CORS ( :fu: ) and more :exclamation:
    Cors friendly
    Prettified
    Added chrome-extension stuff
    -Get and send requests from chrome extension done

--- chrome-extension.js
+++ chrome-extension.js
@@ -1,8 +1,8 @@
 //probably won't support group chat for now
+var serverUrl = "http://127.0.0.1:3000";
 
-
-function extractUsername = function(link){
-	return link.substring(link.lastIndexOf("/")+1);
+function extractUsername = function(link) {
+    return link.substring(link.lastIndexOf("/") + 1);
 }
 
 //currently only effects first chatbox
@@ -14,34 +14,61 @@ var tempscr = chat.getElementsByClassName('fbNubFlyoutBody scrollable')[0];
 
 var oldScroll = tempscr.scrollTop;
 
-var el = document.getElementById('mjkaufer2cool'), elClone = el.cloneNode(true);
+var el = document.getElementById('mjkaufer2cool'),
+    elClone = el.cloneNode(true);
 
-el.parentNode.replaceChild(elClone, el);//gets rid of fb's events so hitting enter doesn't send the message to fb
+el.parentNode.replaceChild(elClone, el); //gets rid of fb's events so hitting enter doesn't send the message to fb
 
-var chat = document.getElementsByClassName('fbNubFlyout fbDockChatTabFlyout uiContextualLayerParent')[0];//chatbox
+var chat = document.getElementsByClassName('fbNubFlyout fbDockChatTabFlyout uiContextualLayerParent')[0]; //chatbox
 
 var titlebar = chat.getElementsByClassName('titlebarText')[0];
-var groupChat = titlebar.href.indexOf("messages/conversation-id.") > -1//whether or not it's a group chat
-var chatid = extractUsername(titlebar.href).replace("conversation-id.","");
+var groupChat = titlebar.href.indexOf("messages/conversation-id.") > -1 //whether or not it's a group chat
+var chatid = extractUsername(titlebar.href).replace("conversation-id.", "");
 
 var scr = chat.getElementsByClassName('fbNubFlyoutBody scrollable')[0];
 
-scr.scrollTop = oldScroll;//scrolls to where it was before the cloning
+scr.scrollTop = oldScroll; //scrolls to where it was before the cloning
 
-var input = chat.getElementsByClassName('uiTextareaAutogrow _552m')[0];//where text is put
+var input = chat.getElementsByClassName('uiTextareaAutogrow _552m')[0]; //where text is put
 
 
-input.onkeydown=function(event){
+input.onkeydown = function(event) {
+    if (event.which == 13) {
+        console.log(this.value); //send to db
+        this.value = "";
+    }
+}
 
-	if(event.which==13){
-	console.log(this.value);//send to db
-	this.value="";
-	}
+var link = extractUsername(document.getElementsByClassName('_2dpe _1ayn')[0].href); //gets current user's username
+
+document.URL.substring(document.URL.lastIndexOf("/") + 1)
+
+
+function getChat(convId) {
+    var xmlhttp = new XMLHttpRequest()
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = xmlhttp.responseText; //to be appended into chat
+        }
+    }
+    var url = serverUrl + "/messages?convId=" + convId;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 
 }
 
+function send(message, convId) { //still need a way to generate the convid effectively
+    var xmlhttp = new XMLHttpRequest()
 
-
-var link = extractUsername(document.getElementsByClassName('_2dpe _1ayn')[0].href);//gets current user's username
-
-document.URL.substring(document.URL.lastIndexOf("/")+1)
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = xmlhttp.responseText; //to be appended into chat
+        } else if (xmlhttp.status != 200) {
+            alert("Message did not send - error")
+        }
+    }
+    var url = serverUrl + "/submit?message=" + message + "&from=" + link + "&convId=" + convId;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
--- main.js
+++ main.js
@@ -1,100 +1,106 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var MongoClient = require('mongodb').MongoClient,
-    format = require('util').format;
+	format = require('util').format;
 
 var url = "127.0.0.1";
 var duration = 30000; //30 seconds until messages are removed
 
 function u(v) { //tests if v is undefined or null; returns true if so
-    return v === undefined || v === null;
+	return v === undefined || v === null;
 }
+
+app.use(cors());
 
 MongoClient.connect('mongodb://' + url + '/fbotrc', function(err, db) {
 
-    if (err) throw err;
+	if (err) throw err;
 
-    var chat = db.collection('chat');
+	var chat = db.collection('chat');
 
-    chat.remove({}, function() {
-        chat.insert({
-            message: "First",
-            from: 42,
-            "to": [54],
-            duration: 86,
-            convId: "test",
-            time: new Date(2013, 2, 1, 1, 10)
-        }, function() {});
-        chat.insert({
-            message: "Third",
-            from: 42,
-            "to": [54],
-            duration: 86,
-            convId: "test",
-            time: new Date(2014, 2, 1, 1, 10)
-        }, function() {});
-        chat.insert({
-            message: "Second",
-            from: 42,
-            "to": [54],
-            duration: 86,
-            convId: "test",
-            time: new Date(2013, 8, 1, 1, 10)
-        }, function() {});
-    })
+	chat.remove({}, function() {
+		chat.insert({
+			message: "First",
+			from: 42,
+			"to": [54],
+			duration: 86,
+			convId: "test",
+			time: new Date(2013, 2, 1, 1, 10)
+		}, function() {});
+		chat.insert({
+			message: "Third",
+			from: 42,
+			"to": [54],
+			duration: 86,
+			convId: "test",
+			time: new Date(2014, 2, 1, 1, 10)
+		}, function() {});
+		chat.insert({
+			message: "Second",
+			from: 42,
+			"to": [54],
+			duration: 86,
+			convId: "test",
+			time: new Date(2013, 8, 1, 1, 10)
+		}, function() {});
+	})
 
-    app.get('/', function(req, res) {
-        chat.findOne({}, {}, function(err, r) {
-            res.end(JSON.stringify(r));
-        })
-    });
+	app.get('/', function(req, res) {
+		chat.findOne({}, {}, function(err, r) {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(r));
+		})
+	});
 
-    app.post('/submit/', function(req, res) { //syntax, ?message=message&from=userfbid&convId=conversationId
-        var q = req.query;
-        if (u(q.message) || u(q.from) || u(q.convId)) //something is missing
-            res.status(404).end('You dun goofed.');
+	app.post('/submit/', function(req, res) { //syntax, ?message=message&from=userfbid&convId=conversationId
+		var q = req.query;
+		if (u(q.message) || u(q.from) || u(q.convId)) //something is missing
+			res.status(404).end('You dun goofed.');
 
-        chat.insert({ //puts message in db
-            message: q.message,
-            from: q.from,
-            convId: q.convId,
-            time: duration,
-            sent: new Date()
-        }, function(err, doc) {
-            doc = doc[0]; //only one query
-            console.log("Submitted a message:")
-            console.log(doc);
-            var postId = doc._id;
+		chat.insert({ //puts message in db
+			message: q.message,
+			from: q.from,
+			convId: q.convId,
+			time: duration,
+			sent: new Date()
+		}, function(err, doc) {
+			doc = doc[0]; //only one query
+			console.log("Submitted a message:")
+			console.log(doc);
+			var postId = doc._id;
 
-            setTimeout(function() {
-                chat.remove(postId, function() {
-                    console.log("Removed post " + postId);
-                })
-            }, doc.time);
+			setTimeout(function() {
+				chat.remove(postId, function() {
+					console.log("Removed post " + postId);
+				})
+			}, doc.time);
 
-        });
+		});
 
-        console.log(req.query);
-        res.end(JSON.stringify(req.query));
+		console.log(req.query);
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(req.query));
 
-    });
+	});
 
-    app.get('/messages/', function(req, res) { //?convId=conversationId
-        var q = req.query;
-        if (u(q.convId))
-            res.status(404).end('You dun goofed.');
+	app.get('/messages/', function(req, res) { //?convId=conversationId
+		var q = req.query;
+		if (u(q.convId))
+			res.status(404).end('You dun goofed.');
 
-        chat.find({
-            convId: q.convId
-        }, function(err, doc) {
-            doc = doc.sort({
-                time: 1
-            }).toArray(function(err, arr) {
-                res.end(JSON.stringify(arr));
-            });
-        })
-    })
+		chat.find({
+			convId: q.convId
+		}, function(err, doc) {
+			doc = doc.sort({
+				time: 1
+			}).toArray(function(err, arr) {
+				res.setHeader('Content-Type', 'application/json');
+				res.end(JSON.stringify(arr));
+			});
+		})
+	})
 
-    app.listen(3000);
+	app.listen(3000);
 
 });
